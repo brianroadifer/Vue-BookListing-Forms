5abdf57f53fc8c82c5fc3cec80c1d5048d07f410
Brian Roadifer <brnrdfr@gmail.com>
2020-06-24 17:58:19 -0600

    Adding a Search Functionality

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -1,8 +1,9 @@
 <template>
   <div>
     <h1>{{title}}</h1>
+    <input v-model="searchInput" type="text" placeholder="Search Books" />
     <ul>
-      <book-item v-for="book in books" :key="book.id" :book="book"></book-item>
+      <book-item v-for="book in searchedBooks" :key="book.id" :book="book"></book-item>
     </ul>
     <hr />
     <h2>Filtered Books By Ownership</h2>
@@ -28,6 +29,7 @@ export default {
   data() {
     return {
       title: "All Books",
+      searchInput: "",
       filters: ["bought", "borrowed"],
       holding: "bought",
       states: ["Want to Read", "Read", "Reading"],
@@ -60,6 +62,12 @@ export default {
   computed: {
     filteredBooks() {
       return _.filter(this.books, ["ownership", this.holding]);
+    },
+    searchedBooks() {
+      const searchFilter = book => {
+        return book.title.toLowerCase().match(this.searchInput.toLowerCase());
+      };
+      return _.filter(this.books, searchFilter);
     }
   },
   methods: {
